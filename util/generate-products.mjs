249810887407d52e69337911af4f1f9dcf2e2934
Fernando Lucio Canizo <fernandocanizo@gmail.com>
2023-08-29 17:00:58 -0400

    Add fake data generator
    Also:
    - ignore produced file on git
    - add npm script to run the generator
    
    On run the script will generate 50,000 fake products with the minimun
    required information. File gets overwritten.
    
    Used ESM to be able to use top level await.

---
+++ util/generate-products.mjs
@@ -0,0 +1,25 @@
+import { faker } from '@faker-js/faker'
+
+import { writeFile } from 'fs/promises'
+
+const InventoryTypeId = '791a6654-c5f2-11e6-aad6-2c56dc130c0d'
+const data = []
+
+for (let i = 0; i < 50_000; i++) {
+	data.push({
+		name: faker.commerce.productName(),
+		code: faker.number.int(),
+		InventoryTypeId,
+		InternalCodeTypeId: null,
+		ProductVersions: [{
+			status: 'waiting-for-creation',
+			code: faker.number.int(),
+			isDefaultVersion: true,
+			InventoryTypeId,
+			InternalCodeTypeId: null,
+			position: 0,
+		}],
+	})
+}
+
+await writeFile('data.json', JSON.stringify(data, null, 2), { encoding: 'utf8' })
